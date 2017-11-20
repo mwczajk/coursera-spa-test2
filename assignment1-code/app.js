9bece7e74bdc0809b26ae27f9a9772a6f37c5265
Marek Czajkowski <marek.czajkowski@computaris.com>
2017-11-20 23:43:04 +0100

    Assignment1

---
+++ assignment1-code/app.js
@@ -0,0 +1,33 @@
+(function () {
+'use strict';
+
+angular.module('MsgApp', [])
+.controller('MsgController', MsgController);
+
+MsgController.$inject = ['$scope'];
+function MsgController($scope) {
+  $scope.lunchMenu = "";
+  $scope.fontColor="red";
+
+  $scope.countDishes = function () {
+    if ($scope.lunchMenu=="") {
+      $scope.message = "Please enter data first";
+      $scope.fontColor = "red";
+      return;
+    }
+    $scope.fontColor = "green";
+    var splittt = $scope.lunchMenu.split(',');
+    var countItms=0;
+    for (var i = 0; i < splittt.length; i++) {
+      if (splittt[i].length > 0) { countItms = countItms+1;}
+    //Do something
+}
+    if (countItms < 4) {
+      $scope.message = "Enjoy!";
+    } else {
+      $scope.message = "Too much!";
+    }
+  };
+}
+
+})();
